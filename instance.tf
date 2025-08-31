5b1237adc4e6697e5ac1c91d3dc68736419e4225
Dhiraj Mali <95579611+dhirajmali17@users.noreply.github.com>
2025-08-31 16:39:23 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -6,17 +6,20 @@ terraform {
     }
   }
 }
+
 provider "aws" {
-  region = "ap-south-1"      
+  region = "ap-south-1"
 }
+
 resource "aws_instance" "myinstance" {
-	 ami = "ami-02d26659fd82cf299"
-	 instance_type = "t2.micro"
-	 subnet_id = "subnet-03bfdf85fe22a8020"
-	 associate_public_ip_address = true
-	 key_name = "dhiraj"
-	 count = 1
-	  tags = {
+  ami                         = "ami-02d26659fd82cf299"
+  instance_type               = "t2.micro"
+  subnet_id                   = "subnet-03bfdf85fe22a8020"
+  associate_public_ip_address = true
+  key_name                    = "dhiraj"
+  count                       = 1
+
+  tags = {
     Name = "tf-example"
-  }
+  }
 }
