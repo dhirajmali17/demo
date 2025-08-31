8c1081e61cd15e5ad03805fce1f5513899390fad
Dhiraj Mali <95579611+dhirajmali17@users.noreply.github.com>
2025-08-31 17:22:16 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -9,8 +9,8 @@ terraform {
 
 provider "aws" {
   region = "ap-south-1"
-  aws_access_key_id = AKIATOQWCSIWAD27V3NM
-  aws_secret_access_key = 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
+  aws_access_key_id = "AKIATOQWCSIWAD27V3NM"
+  aws_secret_access_key = "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"
 }
 
 resource "aws_instance" "myinstance2" {
